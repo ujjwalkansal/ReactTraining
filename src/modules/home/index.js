266d027de9a10dacfb8936b0953ad71796119dfc
ujjwalkansal <ujjwal.kansal@appinventiv.com>
2021-06-02 11:35:54 +0530

    added index.css to home module for styling of home component

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import "./index.css";
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>{"Hello from Home component"}</h1>
-        <h2>{"Howdy Appinventiv"}</h2>
+      <div className="homeContainer">
+        <h1 className="h1tag">{"Hello from Home component"}</h1>
+        <h2 className="h2tag">{"Howdy Appinventiv??"}</h2>
       </div>
     );
   }
